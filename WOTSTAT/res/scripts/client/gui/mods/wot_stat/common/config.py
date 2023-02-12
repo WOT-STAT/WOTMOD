bdbd954c9904f98914b47266942dec9fbedd8bd6
Andrei <soprachev@mail.ru>
2023-02-12 06:15:06 +0300

    reident

--- WOTSTAT/res/scripts/client/gui/mods/wot_stat/common/config.py
+++ WOTSTAT/res/scripts/client/gui/mods/wot_stat/common/config.py
@@ -5,32 +5,37 @@ from ..utils import print_log
 
 
 class Config:
-    config = {}
-    defaultParams = {
-        'version': '1.0.0.0',
-        'initBattleURL': 'https://dev.wotstat.soprachev.com/api/events/OnBattleStart',
-        'eventURL': 'https://dev.wotstat.soprachev.com/api/events/send'
-    }
+  config = {}
+  defaultParams = {
+    'version': '0.0.0.0',
+    'initBattleURL': 'https://dev.wotstat.soprachev.com/api/events/OnBattleStart',
+    'eventURL': 'https://dev.wotstat.soprachev.com/api/events/send'
+  }
 
-    def __init__(self, ConfigPath, DefaultParams=None):
-        if DefaultParams:
-            self.defaultParams = DefaultParams
-            
-            
+  def __init__(self, ConfigPath, DefaultParams=None):
+    self.config = {}
+    if DefaultParams:
+      self.defaultParams = DefaultParams
 
-        if os.path.exists(ConfigPath):
-            with open(ConfigPath, "r") as f:
-                try:
-                    self.config = json.loads(f.read())
-                    print_log('found new config:')
-                    print_log(self.config)
-                except Exception, e:
-                    print_log('load config error')
-                    print_log(e)
+    if os.path.exists(ConfigPath):
+      with open(ConfigPath, "r") as f:
+        try:
+          self.config = json.loads(f.read())
+          print_log('found new config:')
+          print_log(self.config)
+        except Exception, e:
+          print_log('load config error')
+          print_log(e)
 
-    def get(self, key):
-        return self.config[key] if key in self.config else self.defaultParams[
-            key] if key in self.defaultParams else None
+    try:
+      # noinspection PyUnresolvedReferences
+      self.config['version'] = version
+    except Exception, e:
+      pass
+
+  def get(self, key):
+    return self.config[key] if key in self.config else self.defaultParams[
+      key] if key in self.defaultParams else None
 
 
 Config = Config
